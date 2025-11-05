8423f8ae33c067d12ade9eea6f03c22d54d00933
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-11-05 15:21:28 +0300

    add rate limiter

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,16 @@
 import express from "express";
-
+import { rateLimit } from 'express-rate-limit';
 const app = express();
 
+const limit  = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+	limit: 100,
+	standardHeaders: 'draft-8', 
+	legacyHeaders: false, 
+	ipv6Subnet: 56,
+})
 app.use(express.json());
-
+app.use(limit);
 app.get("/", (req, res) => {
     res.send("welcome to the server of home owner app");
 });
